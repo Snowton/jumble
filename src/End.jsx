baada64cc2c897da025af5b7a4225a6058d5dcd2
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2020-04-24 20:07:58 -0400

    add media query for buttons, add favicon

--- src/End.jsx
+++ src/End.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 function End(props) {
-    return <div className="end"><h2>Well Done!</h2><button className="btn btn-lg btn-outline-dark"
+    const matches = useMediaQuery('(max-width:768px)');
+
+    return <div className="end"><h2>Well Done!</h2><button 
+    className={matches ? "btn btn-lg btn-dark" : "btn btn-lg btn-outline-dark"}
     onClick={props.reset}>PLAY AGAIN</button></div>
 }
 
--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 function Feedback(props) {
+    const matches = useMediaQuery('(max-width:768px)');
+
     if (props.success) {
         return <div>
         <p>Good job! You got it right!</p>
-        <button className="btn btn-outline-dark" onClick={props.nextWord}>NEXT WORD</button>
+        <button className={matches ? "btn btn-dark" : "btn btn-outline-dark"} onClick={props.nextWord}>NEXT WORD</button>
         </div>;
     } else {
         return <div>Not quite, try again</div>;
